192a243528225747e1079244947029e7dd849df3
mohsen kermanifar <mohsen.kermanifar@gmail.com>
2023-06-01 12:14:59 +0330

    add file upload

--- src/pages/home/components/UploadImage.tsx
+++ src/pages/home/components/UploadImage.tsx
@@ -1,9 +1,100 @@
-import React from 'react';
+import { FC, useCallback, memo } from 'react';
+import { useDropzone } from 'react-dropzone';
+import Stack from '@mui/material/Stack';
 
-const UploadImage = () => {
+import { colorPalette } from '@uiKits/colors';
+import { Button } from '@uiKits/button/Button';
+
+const UploadImage: FC<UploadImageProps> = ({ img, onUploaded, onRemove }) => {
+
+  const onDrop = useCallback((_acceptedFiles: any) => {
+    if (_acceptedFiles.length > 0) {
+
+      _acceptedFiles.map((_file: File) => {
+
+        const formData = new FormData();
+        formData.append('image', _file);
+
+        onUploaded(_file);
+
+      });
+    }
+  }, []);
+
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+    accept: { 'image/*': ['.jpeg', '.png', '.jpg'], },
+    onDrop,
+    maxSize: 2000000,
+    maxFiles: 1
+  });
+
   return (
-    <div>UploadImage</div>
+    <>
+      <input {...getInputProps()} />
+
+      <Stack
+        flexDirection={'row'}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+        gap={'31px'}
+      >
+        <Stack
+          alignItems={'center'}
+          justifyContent={'center'}
+          width={96}
+          height={96}
+          borderRadius={'50%'}
+          bgcolor={colorPalette.gray70}
+          sx={{
+            cursor: 'pointer',
+            backgroundImage: `url(${img ? URL.createObjectURL(img as File) : ''})`,
+            backgroundPosition: '50% 50%',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover'
+          }}
+        />
+
+        <Stack gap={'8px'} flex={1}>
+          <Button
+            variant={'contained'}
+            type={'button'}
+            backgroundColor={colorPalette.blue800}
+            LabelColor={colorPalette.white}
+            fullWidth={true}
+            sx={{
+              fontSize: 16,
+              fontWeight: 700
+            }}
+            {...getRootProps({ className: 'dropzone' })}
+          >
+            Pick an Passport Image
+          </Button>
+
+          <Button
+            variant={'outlined'}
+            type={'button'}
+            borderColor={'rgba(39, 64, 82, 0.2)'}
+            LabelColor={colorPalette.blue800}
+            fullWidth={true}
+            sx={{
+              fontSize: 16,
+              fontWeight: 700
+            }}
+            onClick={() => onRemove()}
+          >
+            Remove
+          </Button>
+        </Stack>
+
+      </Stack>
+    </>
   );
 };
+
+export interface UploadImageProps {
+  img: File | null;
+  onUploaded: (file: File) => void;
+  onRemove: () => void;
+}
 
 export default UploadImage;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,31 @@
+import { useState } from 'react';
+import Stack from '@mui/material/Stack';
+
+import UploadImage from './components/UploadImage';
+import { colorPalette } from '@uiKits/colors';
+
 const HomePage = () => {
+
+    const [img, setImg] = useState<File | null>(null);
+
     return (
-        <div>HomePage</div>
+        <Stack
+            alignItems={'center'}
+            pt={'80px'}
+        >
+            <Stack
+                width={620}
+                bgcolor={colorPalette.white}
+                borderRadius={'20px'}
+                p={'37px'}
+            >
+                <UploadImage
+                    img={img}
+                    onUploaded={(_file: File) => setImg(_file)}
+                    onRemove={() => setImg(null)}
+                />
+            </Stack>
+        </Stack>
     );
 };
 
